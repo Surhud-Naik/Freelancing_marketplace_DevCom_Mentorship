bebf4bc218cac140bff5ce8dc1d203c68d04b43b
SurhudNaik <naiksurhud@gmail.com>
2026-01-17 14:38:53 +0530

    setup-react-django-tailwind

---
+++ frontend/freelancing_frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className = "m-4 p-4 bg-yellow-200 font-bold rounded-l"> Hello</div>
+    </>
+  )
+}
+
+export default App
